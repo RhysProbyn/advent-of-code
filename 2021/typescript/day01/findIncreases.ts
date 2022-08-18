2ad1feee53de20883702976c9c9dbddd7c18fdb5
RhysProbyn <rhysprobyn@gmail.com>
2022-08-19 00:44:16 +1000

    playing with typescript

--- 2021/typescript/day01/findIncreases.ts
+++ 2021/typescript/day01/findIncreases.ts
@@ -1,11 +1,27 @@
 import * as fs from 'fs';
-const file = fs.readFileSync('./input/depths.csv','utf8');
-const depthArray = file.split("\n").map(Number)
+
+const file = fs.readFileSync('./input/testInput.csv','utf8');
+
+// splitting text and mapping to Number and filtering out bad inputs
+// const depthArray = file.split("\n").map((line)=>{
+//   if(Boolean(line)){
+//     return(Number(line))
+//   }
+// }).filter((line)=>Boolean(line)) as number[] 
+
+
+// doing the same on one line - wowzer
+const depthArray = file.split("\n").map((val1) => Boolean(val1) ? Number(val1):null).filter((val2) => Boolean(val2)) as number[] 
+
+console.log(depthArray)
+
 let increases = 0
+
 for (let index = 1; index < depthArray.length; index++) {
   const previousItem = depthArray[index-1]
   if (previousItem < depthArray[index]) {
     increases += 1
   }
 }
+
 console.log(increases)
