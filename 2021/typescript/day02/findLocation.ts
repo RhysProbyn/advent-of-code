75036a44f57de267f3e504cd1f7e74974cd60916
Rhys.Probyn <rhys.probyn@arup.com>
2022-08-25 17:02:59 +1000

    day02 part2

--- 2021/typescript/day02/findLocation.ts
+++ 2021/typescript/day02/findLocation.ts
@@ -8,22 +8,24 @@ const movements:[string,number][]=file
 .map((line)=>line.replace("\r",""))
 .map((line)=>line.split(" ")).map(move=>[move[0],Number(move[1])])
 
-let depth = 0
+let aim = 0
 let horizontal = 0
+let depth = 0
 
-const moveForward = (val:number) => horizontal += val
-const moveUp = (val:number) => depth -= val
-const moveDown = (val:number) => depth += val
+const moveForward = (val:number) => {
+  horizontal += val
+  depth += aim*val
+}
+const aimUp = (val:number) => aim -= val
+const aimDown = (val:number) => aim += val
 
 const moveMap:any = {
   "forward":moveForward,
-  "up":moveUp,
-  "down":moveDown
+  "up":aimUp,
+  "down":aimDown
 }
 
-movements.forEach(move => {
-  moveMap[move[0]](move[1])  
-});
+movements.forEach(move => moveMap[move[0]](move[1]));
 
 console.log(depth,horizontal)
 console.log(depth*horizontal)
