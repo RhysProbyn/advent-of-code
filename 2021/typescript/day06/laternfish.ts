0f7cfef0cad42f8b4050e960903427dddb6545ef
Rhys.Probyn <rhys.probyn@arup.com>
2022-08-29 18:24:46 +1000

    day06 final

--- 2021/typescript/day06/laternfish.ts
+++ 2021/typescript/day06/laternfish.ts
@@ -11,61 +11,15 @@ const inputFile = (arg: string) => {
 const file = fs.readFileSync(inputFile(process.argv[2]), "utf8");
 const initialState: number[] = file.split(",").map((x) => parseInt(x.trim()));
 
-// console.log(initialState);
-const handleCycle = (day: number) => {
-  if (day > 0) {
-    return [day - 1];
-  } else {
-    return [6, 8];
-  }
-};
+const ageCount = new Array(9).fill(0);
+initialState.map((age) => (ageCount[age] += 1));
 
-const findNumberOfLaternfish = (iterations: number) => {
-  let updatedStateArray = initialState.map((val) => [val, 0]);
-  // console.log(updatedStateArray);
-  // console.log(updatedStateArray);
-
-  let total = 0;
-  let totalNumberOfArrays = updatedStateArray.length;
-  let updatedState = [updatedStateArray[0][0]];
-  for (let j = 0; j < totalNumberOfArrays; j++) {
-    let subIterations = updatedStateArray[j][1];
-    for (let i = subIterations; i < iterations; i++) {
-      updatedState = updatedState.flatMap(handleCycle);
-      const stateLength = updatedState.length;
-      // console.log(stateLength);
-
-      if (stateLength > 3) {
-        console.log("i", i);
-
-        const updatedStateAdds = updatedState.map((val) => [val, i]);
-        updatedStateArray = updatedStateArray.concat(updatedStateAdds);
-        totalNumberOfArrays += stateLength - 1;
-        // console.log(updatedStateArray);
-        // console.log("j", j);
-        total += updatedState.length;
-
-        break;
-      }
-    }
-    console.log(updatedState);
-  }
-  total += updatedState.length;
-  return total;
-};
-
-const numberOfCycles = 18;
-console.log("Attempting to perform " + numberOfCycles + " cycles");
-const start = Date.now();
-// for (var i = 0; i < numberOfComputations; i++) {
-//   findWindowIncreases(depthArray);
-// }
-console.log(findNumberOfLaternfish(numberOfCycles));
-const milliseconds = Date.now() - start;
-
-console.log(
-  numberOfCycles +
-    " computations performed in " +
-    milliseconds +
-    " milliseconds"
-);
+console.log(ageCount);
+const iterations = 256;
+for (let i = 0; i < iterations; i++) {
+  const zeroAge = ageCount.shift();
+  ageCount[8] = zeroAge;
+  ageCount[6] += zeroAge;
+  // console.log(i);
+}
+console.log(ageCount.reduce((total, val) => total + val));
