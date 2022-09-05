3275fe4de1b1ea08bf1cf0cfb8ae9d39744ed8ce
Rhys.Probyn <rhys.probyn@arup.com>
2022-09-05 20:31:59 +1000

    day09 in-progress

--- 2021/typescript/day09/findLowPoints.ts
+++ 2021/typescript/day09/findLowPoints.ts
@@ -14,62 +14,131 @@ const depths: number[][] = file
   .map((x) => x.split("").map((y) => parseInt(y.trim())));
 console.log(depths);
 
-const transposeMatrix = (matrix: any[][]) =>
-  matrix[0].map((_, columnIndex) =>
-    matrix.map((_, rowIndex) => matrix[rowIndex][columnIndex])
-  );
+const part1 = (depthMatrix: number[][]) => {
+  let sum = 0;
+  for (const [row, rowArray] of depthMatrix.entries()) {
+    for (const [column, value] of rowArray.entries()) {
+      let right = 9;
+      let left = 9;
+      let up = 9;
+      let down = 9;
+      try {
+        right = rowArray[column + 1];
+        right = right === undefined ? 9 : right;
+      } catch {
+        right = 9;
+      }
+      try {
+        left = rowArray[column - 1];
+        left = left === undefined ? 9 : left;
+      } catch {
+        left = 9;
+      }
+      try {
+        up = depthMatrix[row - 1][column];
+        up = up === undefined ? 9 : up;
+      } catch {
+        up = 9;
+      }
+      try {
+        down = depthMatrix[row + 1][column];
+        down = down === undefined ? 9 : down;
+      } catch {
+        down = 9;
+      }
+      console.log("value", value);
 
-const flippedDepths: number[][] = transposeMatrix(depths);
-let sum = 0;
-for (const [row, rowArray] of depths.entries()) {
-  for (const [column, value] of rowArray.entries()) {
-    let right = 10;
-    let left = 10;
-    let up = 10;
-    let down = 10;
-    try {
-      right = rowArray[column + 1];
-      right = right === undefined ? 10 : right;
-    } catch {
-      right = 10;
-    }
-    try {
-      left = rowArray[column - 1];
-      left = left === undefined ? 10 : left;
-    } catch {
-      left = 10;
-    }
-    try {
-      up = depths[row - 1][column];
-      up = up === undefined ? 10 : up;
-    } catch {
-      up = 10;
-    }
-    try {
-      down = depths[row + 1][column];
-      down = down === undefined ? 10 : down;
-    } catch {
-      down = 10;
-    }
-    console.log("value", value);
+      console.log(8, up, 8);
+      console.log(left, value, right);
+      console.log(8, down, 8);
 
-    console.log(8, up, 8);
-    console.log(left, value, right);
-    console.log(8, down, 8);
+      if (value < up && value < down && value < left && value < right) {
+        console.log("hit");
 
-    if (value < up && value < down && value < left && value < right) {
-      console.log("hit");
-
-      console.log(value);
-      sum += 1 + value;
-      // console.log(flippedDepths[column][row]);
-      // if (
-      //   flippedDepths[column][row + 1] < value &&
-      //   flippedDepths[column][row - 1] < value
-      // ) {
-      //   console.log("haha");
-      // }
+        console.log(value);
+        sum += 1 + value;
+      }
     }
   }
+  return sum;
+};
+interface PointObject {
+  x: number;
+  y: number;
 }
-console.log(sum);
+const Point = (x: number, y: number): PointObject => {
+  return { x: x, y: y };
+};
+const point = Point(1, 1);
+
+console.log(point);
+
+const expand = (location: PointObject, depthMatrix: number[][]) => {
+  let sum = 0;
+  let points = [location];
+  let expanded = true;
+  let iterationPoints: PointObject[] = [];
+  while (expanded) {
+    expanded = false;
+    for (const point of points) {
+      const rowArray = depthMatrix[point.y];
+      const row = point.y;
+      const column = point.x;
+      let right = 9;
+      let left = 9;
+      let up = 9;
+      let down = 9;
+      try {
+        right = rowArray[column + 1];
+        right = right === undefined ? 9 : right;
+        if (right !== 9) {
+          iterationPoints.push(Point(row, column + 1));
+          expanded = true;
+        }
+      } catch {
+        right = 9;
+      }
+      try {
+        left = rowArray[column - 1];
+        left = left === undefined ? 9 : left;
+        if (left !== 9) {
+          iterationPoints.push(Point(row, column - 1));
+          expanded = true;
+        }
+      } catch {
+        left = 9;
+      }
+      try {
+        up = depthMatrix[row - 1][column];
+        up = up === undefined ? 9 : up;
+        if (up !== 9) {
+          iterationPoints.push(Point(row - 1, column));
+          expanded = true;
+        }
+      } catch {
+        up = 9;
+      }
+      try {
+        down = depthMatrix[row + 1][column];
+        down = down === undefined ? 9 : down;
+        if (down !== 9) {
+          iterationPoints.push(Point(row + 1, column));
+          expanded = true;
+        }
+      } catch {
+        down = 9;
+      }
+      // console.log("value", value);
+
+      // console.log(8, up, 8);
+      // console.log(left, value, right);
+      // console.log(8, down, 8);
+    }
+    points = iterationPoints;
+    console.log(iterationPoints);
+  }
+  return sum;
+};
+
+console.log(part1(depths));
+expand(Point(4, 4), depths);
